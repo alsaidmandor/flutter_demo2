2f0dfe012f923c5f60f6ee5502dba08d6644f2dd
alsaidmandor <alsaidmandor8888@gmail.com>
2021-03-18 12:54:01 +0200

    init email page

---
+++ lib/screen/app_sign_in/email_sign_in_page.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Time Tracker',
+        ),
+        centerTitle: true, // this is all you need
+        elevation: 2.0,
+      ),
+      body: Container(),
+    );
+  }
+}
