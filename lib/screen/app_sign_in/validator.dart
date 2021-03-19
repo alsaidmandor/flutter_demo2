482a339cbd0c30cee66fa0edbad4d9a6a096d3f4
alsaidmandor <alsaidmandor8888@gmail.com>
2021-03-19 11:22:27 +0200

    add validator.dart

---
+++ lib/screen/app_sign_in/validator.dart
@@ -0,0 +1,22 @@
+
+abstract class StringValidator
+{
+  bool isValid (String value) ;
+}
+
+class NotEmptyStringValidator implements StringValidator
+{
+  @override
+  bool isValid(String value) {
+    return value.isNotEmpty ;
+  }
+
+
+}
+
+// this class is uses as Mixin
+class EmailAndPasswordValidators
+{
+  final StringValidator emailValidator = NotEmptyStringValidator() ;
+  final StringValidator passwordValidator = NotEmptyStringValidator() ;
+}
