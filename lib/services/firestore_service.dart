80b9099b0991abe1a06a32325b307b25c236040d
alsaidmandor <alsaidmandor8888@gmail.com>
2021-03-22 18:53:15 +0200

    add method for delete form fireStore

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -17,7 +17,14 @@ class FireStoreService {
         snapshot.docs.map((snapshot) => builder(snapshot.data() , snapshot.id,)).toList());
   }
 
-  Future<void> setData({String path, Map<String, dynamic> data}) async {
+  Future<void> deleteData ({@required String path}) async
+  {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('delete : $path') ;
+    await reference.delete();
+  }
+
+  Future<void> setData({@required String path, @required Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path : $data');
     await reference.set(data);
